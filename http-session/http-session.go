401a618c445cb384960840014adaf31d3fddcd05
Victor Garcés <victorgarcesgg@gmail.com>
2021-10-04 21:56:46 -0400

    Added Redis as session handler

--- http-session/http-session.go
+++ http-session/http-session.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/sessions"
+	"gopkg.in/boj/redistore.v1"
 )
 
 const (
@@ -13,10 +13,14 @@ const (
 	CONN_PORT = "8080"
 )
 
-var store *sessions.CookieStore
+var store *redistore.RediStore
+var err error
 
 func init() {
-	store = sessions.NewCookieStore([]byte("secret-key"))
+	store, err = redistore.NewRediStore(10, "tcp", ":6379", "", []byte("secret-key"))
+	if err != nil {
+		log.Fatal("error getting redis store : ", err)
+	}
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -38,7 +42,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 func login(w http.ResponseWriter, r *http.Request) {
 	session, _ := store.Get(r, "session-name")
 	session.Values["authenticated"] = true
-	session.Save(r, w)
+	if err = session.Save(r, w); err != nil {
+		log.Fatalf("Error saving session: %v", err)
+	}
 	fmt.Fprintln(w, "You have successfully logged in")
 }
 
@@ -54,6 +60,7 @@ func main() {
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/logout", logout)
 	err := http.ListenAndServe(CONN_HOST+":"+CONN_PORT, nil)
+	defer store.Close()
 	if err != nil {
 		log.Fatal("error starting http server : ", err)
 		return
